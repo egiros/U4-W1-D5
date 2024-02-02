b860b6f6ad2b6b6b85dffc1d03d711cd3da4910f
egiros <148718730+egiros@users.noreply.github.com>
2024-02-02 16:25:17 +0100

    summary

---
+++ U4-W1-D5/Contribuente.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U4_W1_D5
+{
+    class Contribuente
+    {
+        // Proprietà della classe
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public DateTime DataNascita { get; set; }  // DateTime fornisce metodi e proprietà per manipolare e ottenere informazioni sulla data e l'ora correnti.
+        public string CodiceFiscale { get; set; }
+        public char Sesso { get; set; }
+        public string ComuneResidenza { get; set; }
+        public decimal RedditoAnnuale { get; set; }
+
+
+
+        // Costruttore della classe
+        public Contribuente(string nome, string cognome, DateTime datanascita, string cdf, char sex, string comuneresidenza, decimal redditoannuale)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            DataNascita = datanascita;
+            CodiceFiscale = cdf;
+            Sesso = sex;
+            ComuneResidenza = comuneresidenza;
+            RedditoAnnuale = redditoannuale;
+        }
+
+
+
+        // Metodo per calcolare l'imposta in base agli scaglioni di reddito
+
+        /*
+         
+               SCAGLIONI DI REDDITO                 ALIQUOTA E IMPOSTA DOVUTA
+            Reddito da 0 a 15.000                   aliquota 23%
+            Reddito da 15.001 a 28.000              3.450 + aliquota 27% sulla parte eccedente i 15.000
+            Reddito da 28.001 a 55.000              6.960 + 38% sulla parte eccedente i 28.000
+            Reddito da 55.001 a 75.000              17.220 + 41% sulla parte eccedente i 55.000
+            Redduti oltre i 75.001                  25.420 + 43% sulla parte eccedente i 75.000
+
+            nella condizione parto dal massimo e sottraggo il minimo degli scaglioni.
+            imposta = Imposta dovuta + RedditoAnnuale(input) x % aliquota (usare m per i decimal)
+         */
+        public decimal CalcolaImposta()
+        {
+            decimal imposta = 0;
+
+            if (RedditoAnnuale <= 15000)
+            {
+                imposta = RedditoAnnuale * 0.23m;
+            }
+            else if (RedditoAnnuale <= 28000)
+            {
+                imposta = 3450 + (RedditoAnnuale - 15000) * 0.27m;
+            }
+            else if (RedditoAnnuale <= 55000)
+            {
+                imposta = 6960 + (RedditoAnnuale - 28000) * 0.28m;
+            }
+            else if (RedditoAnnuale <= 75000)
+            {
+                imposta = 17220 + (RedditoAnnuale - 55000) * 0.41m;
+            }
+            else
+            {
+                imposta = 25420 + (RedditoAnnuale - 75000) * 0.43m;
+            }
+            return imposta;
+        }
+
+        // Metodo per ottenere una stringa le informazioni necessarie del contribuente
+
+        public string OttieniInfoContribuente()
+        {
+            return $"\nContribuente: {Nome} {Cognome}" +
+                  $"\nNato il {DataNascita.ToString("dd/MM/yyyy")}, ({Sesso})" +
+                  $"\nResidente in {ComuneResidenza}" +
+                  $"\nCodice Fiscale: {CodiceFiscale}";
+        }
+    }
+}
--- U4-W1-D5/Program.cs
+++ U4-W1-D5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,102 @@ namespace U4_W1_D5
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;  // Soluzione trovata online per la visualizzazione dei caratteri tipo €
+
+            // Menu per interagire con l'utente in input
+
+            Console.WriteLine("============================================================================");
+            Console.WriteLine("***************BENVENUTO!***************");
+            Console.WriteLine("Inserisci i dati richiesti dalla nostra APP per il calcolo dell'imposta da versare");
+            Console.WriteLine("============================================================================");
+
+            string nome, cognome, cdf, comuneresidenza;
+            DateTime datanascita;
+            char sex;
+            decimal redditoannuale;
+
+            do
+            {
+                Console.WriteLine("\n\nInserisci il NOME del contribuente: ");
+                nome = Console.ReadLine().ToUpper(); // ToUpper trasforma tutte le lettere in maiuscolo
+
+                Console.WriteLine("\nInserisci il COGNOME del contribuente: ");
+                cognome = Console.ReadLine().ToUpper();
+
+                while (true) // questo ciclo ripete l'inserimento della data di nascita fichè non rispetta il formato corretto
+                {
+                    Console.WriteLine("\nInserisci la DATA DI NASCITA del contribuente: (gg/mm/aaaa)");
+
+                    try
+                    {
+                        datanascita = DateTime.Parse(Console.ReadLine());
+                        break; // esce dal ciclo se la data è nel formato corretto
+                    }
+                    catch (FormatException)  // soluzione suggerita direttamente da VS
+                    {
+                        Console.WriteLine("\n ATTENZIONE!!!!\nFormato DATA non valido. \nRiprova facendo attenzione a rispettare GG/MM/AAAA");
+                    }
+
+                }
+
+                while (true)
+                {
+                    Console.WriteLine("\nInserisci il CODICE FISCALE del contribuente: ");
+                    cdf = Console.ReadLine().ToUpper();
+
+                    if (cdf.Length == 16)
+                    {
+                        // Esce dal ciclo se la lunghezza del CDF è uguale di 16
+                        break;
+                    }
+                    else
+                    { 
+                        Console.WriteLine("\n ATTENZIONE!!!!\nFormato CDF non valido. \nRiprova, il CODICE FISCALE deve avere una lunghezza di 16 caratteri");
+                    }
+                }
+
+                   
+
+                Console.WriteLine("\nInserisci il SESSO del contribuente: (M/F)");
+                sex = char.Parse(Console.ReadLine().ToUpper()); ;
+
+                Console.WriteLine("\nInserisci il COMUNE DI RESIDENZA: ");
+                comuneresidenza = Console.ReadLine().ToUpper();
+
+                Console.WriteLine("\nInserisci il REDDITO ANNUALE del contribuente: \n€ ");
+                redditoannuale = decimal.Parse(Console.ReadLine());
+               
+
+                Console.WriteLine("\nI tuoi dati sono corretti?");
+                Console.WriteLine("\nPremi il tasto Y per confermare N per inserirli nuovamente.");
+
+            } while (Console.ReadLine().ToUpper() != "Y");
+
+            // Viene creata un'istanza della classe Contribuente con i dati inseriti
+            Contribuente contribuente = new Contribuente(nome, cognome, datanascita, cdf, sex, comuneresidenza, redditoannuale);
+
+            // Viene utilizzato il metodo CalcolaImposta appunto per calcolare l'imposta
+            decimal impostaDaVersare = contribuente.CalcolaImposta();
+
+            // Stampa un riempilogo dei dati e dell'imposta calcolata
+            Console.WriteLine("\n\n============================================================================");
+            Console.WriteLine("CALCOLO DELL'IMPOSTA DA VERSARE: ");
+            Console.WriteLine("============================================================================");
+
+            // Stampa i dettagli del contribuente
+            Console.WriteLine(contribuente.OttieniInfoContribuente());
+
+            // Stampa il reddito dichiarato e l'imposta da versare
+            Console.WriteLine($"\nReddito dichiarato: {contribuente.RedditoAnnuale:C}");  // :C Metodo di valuta per visualizzare €
+            Console.WriteLine($"\nIMPOSTA DA VERSARE: {impostaDaVersare:C}");
+
+
+            Console.WriteLine("\n\n\n\n\n============================================================================");
+            Console.WriteLine("**********PREMI INVIO PER CHIUDERE L'APPLICAZIONE**********");
+            Console.WriteLine("============================================================================");
+            Console.ReadLine();
         }
+
+       
     }
 }
